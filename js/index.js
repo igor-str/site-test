228c937eb387e53b16ecd73623aa48cd12ac1ca4
admin <igorok.strilchuk@gmail.com>
2017-04-20 23:35:59 +0300

    Ще треба медіа запити до форми, футера, банера та меню

--- js/index.js
+++ js/index.js
@@ -10,4 +10,40 @@ $(function(){
         $(this).children('.product-actions').stop(true, true).fadeTo(500, 0);
     });
 
+    $('.wrapper-slider-caruselle').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: true,
+        autoplay: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
 });
